c7a371975492e87e3becf588b814449caf817f47
pratyakshacode <pratyaksh@beingzero.in>
2025-04-06 23:54:34 +0530

    Added toast to the application for use

--- src/pages/Home/LandingPage.tsx
+++ src/pages/Home/LandingPage.tsx
@@ -1,7 +1,9 @@
 import brainImage from '@/assets/brain.svg';
 import { Button } from '@/components/ui/button';
+import { showToast } from '@/utils/toast';
 
 const LandingPage = () => {
+
   return (
     <div className='w-full p-3' style={{ height: '90vh'}}>
        <div className='hero-section flex flex-col md:flex-row gap-0 h-full rounded-3xl backdrop-blur-sm' style={{ background: 'linear-gradient(145deg, rgba(246, 76, 165, 0.8), rgba(0, 153, 255, 0.2), black)'}}>
@@ -9,7 +11,14 @@ const LandingPage = () => {
                 <h1 className='text-white text-4xl mt-10 text-center md:text-6xl '>Enlight Your Future</h1>
                 <h1 className='text-white text-4xl md:text-6xl text-center'>With Us</h1>
                 <h2 className='text-white text-center'>We provide the best courses for your future and for your career</h2>
-                <Button className='w-5/12' variant={'outline'}>Explore Courses</Button>
+                <Button onClick={() => {
+                  showToast({
+                    title: "Hello world",
+                    description: "Exploring the courses with notification",
+                    color: 'lime',
+                    duration: 1000
+                  })
+                }} className='w-5/12' variant={'outline'}>Explore Courses</Button>
             </div>
 
             <div className='right-content-section flex justify-end  w-full m-0'>
